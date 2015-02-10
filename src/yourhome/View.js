b6bba2b221bf18a891f84d02a36328134fb135a0
Markus Bosek <markus.bosek@stud.uni-regensburg.de>
2015-02-10 13:59:32 +0100

    Sortlist added
    sortlist items and checkboxes added

--- src/yourhome/View.js
+++ src/yourhome/View.js
@@ -7,8 +7,26 @@ Yourhome.View = (function () {
         init = function () {
             left = $('#steuerunglinks');
             middle = $('#middle-area');
+            _loadSortItems();
             _loadInfoboard();
             return that;
+        },
+            
+        _loadSortItems = function(){
+            var sort = left.find("#sort-options"),
+                sortItems = ["Alle anzeigen", "Aufgaben", "usw"];
+            _.each(sortItems, function(element){
+                var sortlistItem = document.createElement("li"),
+                    checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                checkbox.setAttribute("checked",true);
+                sortlistItem.appendChild(checkbox);
+                sortlistItem.innerHTML += " " + element;
+                sort.append(sortlistItem);
+            });
+        },
+            
+        _onCheckedChange = function(){
         },
         
         _loadInfoboard = function(){
@@ -31,17 +49,25 @@ Yourhome.View = (function () {
                 input = document.createElement("input");
             submit.innerHTML = "OK";
             input.autofocus = "true";
+            input.className = "Infoboard-New-Entry";
             submit.onclick = function(){
                 _submitInfoEntry(input);
             };
-            entry.appendChild(submit);
             entry.appendChild(input);
+            entry.appendChild(submit);
             middle.append(entry);
             
         },
             
-        _submitInfoEntry = function(input){
-            infoboard.push(input.value);
+        _submitInfoEntry = function(input){    
+            var TESTIMG = new Image();
+            TESTIMG.src = "res/assets/grey.png";
+            var infoboardEntry = {"feature":"inforboard",
+                                  "user-img":TESTIMG,
+                                  "input-text":input.value,
+                                  "user-name":"hello",
+                                 "date": "1.1.111!"};
+            infoboard.push(infoboardEntry);
             $('body').trigger('infoboardChanged',{"infoboard":infoboard});
             _renderInfoboard();
         },
@@ -54,9 +80,18 @@ Yourhome.View = (function () {
         },
         
         _infoContainer = function(element){
-            var container = document.createElement("div");
+            var container = document.createElement("div"),
+                inputText = document.createElement("text"),
+                inputName = document.createElement("text"),
+                inputDate = document.createElement("text");
+            inputText.innerHTML = element["input-text"];
+            inputName.innerHTML = element["user-name"];
+            inputDate.innerHTML = element["date"];
             container.className = "infoboard-entry";
-            container.innerHTML = element;
+            container.appendChild(element["user-img"]);
+            container.appendChild(inputText);
+            container.appendChild(inputName);
+            container.appendChild(inputDate);
             return container;
         };
 
